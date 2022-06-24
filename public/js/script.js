7d415bf0840e66289e1b35a74f3463e7faa12215
Muhammad Raihan Alifiandra <97264840+Re-kun@users.noreply.github.com>
2022-06-24 16:25:39 +0700

    Add files via upload

--- public/js/script.js
+++ public/js/script.js
@@ -2,8 +2,26 @@ let hamburger = document.querySelector('nav input');
 let navItems = document.querySelector('.nav-items');
 
 hamburger.addEventListener('click', function(){
-	navItems.classList.toggle('-top-[44rem]')
-	navItems.classList.toggle('top-16')
-	document.querySelector('body').classList.toggle('fixed')
+	// navItems.classList.toggle('-top-[44rem]')
+	navItems.classList.toggle('-left-[70rem]')
+	navItems.classList.toggle('-left-[0rem]')
+
 })
 
+	var posisiY = 0;
+	var jarak = 20;
+	function smoothScroll(id){
+		var target = document.getElementById(id).offsetTop;
+		var scrollAnimate = setTimeout(function(){
+			smoothScroll(id);
+		}, 5)
+
+		posisiY = posisiY + jarak;
+
+		if (posisiY >= target) {
+			clearTimeout(scrollAnimate);
+			posisiY = 0;
+		}else {
+			window.scroll(0, posisiY)
+		}
+	}
